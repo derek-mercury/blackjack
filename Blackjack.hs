40618c60cc5131222b7fe13fc9e0c0de6c54dbef
Derek <derek@mercury.com>
2022-07-19 12:14:51 -0700

    Add function to find optimal hand value
    - Update variable name of Rank datatype
    - Optimal hand value function determines which Ace value would be best for a hand - 1 or 13 depending on if the the sum > 21

--- Blackjack.hs
+++ Blackjack.hs
@@ -1,3 +1,4 @@
+
 module Blackjack where
 
 data Suit = Clubs
@@ -5,7 +6,7 @@ data Suit = Clubs
             | Hearts
             | Spades
             deriving (Show)
-data Number = Ace
+data Rank = Ace
             | Two
             | Three
             | Four
@@ -20,5 +21,37 @@ data Number = Ace
             | King
             deriving (Show)
 
-data Card = Card Suit Number
+data Card = Card Suit Rank
             deriving (Show)
+
+-- create list of card objects, 13 of each
+-- createDeck = [Card Clubs 1, .., Card Clubs 13]
+
+-- convert Rank to actual Num
+getCardValue:: Rank -> Int
+getCardValue c = case c of 
+    Ace -> 1
+    Two -> 2
+    Three -> 3
+    Four -> 4
+    Five -> 5
+    Six -> 6
+    Seven -> 7
+    Eight -> 8
+    Nine -> 9
+    Ten -> 10
+    Jack -> 11
+    Queen -> 12
+    King -> 13
+    -- c == Ace -> 13
+
+-- get value of hand
+getOptimalHandValue :: Card -> Card -> Int
+getOptimalHandValue (Card _ rank1) (Card _ rank2)
+    -- the only ever time we want to make Ace = 13 is when
+    | (val1 == 1 || val2 == 1) && val1 + val2 + 12 <= 21 = val1 + val2 + 12
+    | otherwise = val1 + val2
+
+    where
+        val1 = getCardValue rank1
+        val2 = getCardValue rank2
